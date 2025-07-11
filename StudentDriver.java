bb09fd533a490a4087629bdfc1dfcaeac51c0501
Yaminee Naresh Chafale <yamineechafale49@gmail.com>
2025-07-11 12:25:25 +0530

    Add files via upload

---
+++ StudentDriver.java
@@ -0,0 +1,12 @@
+class StudentDriver 
+{
+	public static void main(String[] args) 
+	{
+		student s1=new student();
+		s1.displayStudent();
+		
+		student s2=new student();
+		s2.displatStudent();
+		
+	}
+}
