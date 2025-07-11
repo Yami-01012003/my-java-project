24347bd823e5308ca2cf3006157a166ed2f3fdc4
Yaminee Naresh Chafale <yamineechafale49@gmail.com>
2025-07-11 10:49:40 +0530

    Add files via upload

---
+++ Literals.java
@@ -0,0 +1,13 @@
+class Literals
+{
+public static void main(String[]args)
+{
+  System.out.println("Yaminee Chafale");
+  System.out.println(21);
+  System.out.println('Z');
+  System.out.println(34.000009);
+  System.out.println('$');
+  System.out.println('0');
+  System.out.println(0);
+}
+}
